3e9f514e1d02de131cf17b7a904d7a5fb453d59a
eclipseop <mh@otterdev.com>
2022-08-15 08:27:47 -0500

    use creation time for getting last revision to reduce execution time by like 200ms

--- src/main/java/com/eclipseop/osrs/Application.java
+++ src/main/java/com/eclipseop/osrs/Application.java
@@ -8,6 +8,7 @@ import com.google.cloud.functions.Context;
 import com.google.cloud.storage.*;
 import com.google.events.cloud.pubsub.v1.Message;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -20,29 +21,38 @@ public class Application implements BackgroundFunction<Message> {
 
   private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
 
+  private static boolean alreadyHave(List<String> blobs, int revision) {
+    String objectName = "osrs-" + revision + ".jar";
+    return blobs.contains(objectName);
+  }
+
+  private static void downloadAndInsert(Storage storage, int revision) {
+    BlobId blobId = BlobId.of(GCS_BUCKET, String.format("osrs-%d.jar", revision));
+    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+    storage.create(blobInfo, Gamepack.getJarBytes());
+  }
+
   @Override
   public void accept(Message message, Context context) throws Exception {
     Storage storage = StorageOptions.getDefaultInstance().getService();
-    Page<Blob> blobs = storage.list(GCS_BUCKET);
+    Page<Blob> blobPages = storage.list(GCS_BUCKET);
 
-    List<String> blobNames =
-        StreamSupport.stream(blobs.iterateAll().spliterator(), false)
-            .map(Blob::getName)
-            .collect(Collectors.toList());
+    List<Blob> blobs =
+        StreamSupport.stream(blobPages.iterateAll().spliterator(), false).collect(Collectors.toList());
 
-    Optional<Integer> lastRevision =
-        blobNames.stream()
-            .map(str -> str.replaceAll("\\D", ""))
-            .map(Integer::valueOf)
-            .max(Integer::compare);
+    Optional<Blob> lastRevisionBlob =
+        blobs.stream().max(Comparator.comparingLong(BlobInfo::getCreateTime));
 
-    if (lastRevision.isEmpty()) {
+    if (lastRevisionBlob.isEmpty()) {
       LOGGER.warning("Unable to detect last revision!");
       return;
     }
+
+    int lastRevision = Integer.parseInt(lastRevisionBlob.get().getName().replace("\\D", ""));
     LOGGER.info("Last revision: " + lastRevision);
 
-    for (int i = lastRevision.get() - 1; i < lastRevision.get() + 3; i++) {
+    List<String> blobNames = blobs.stream().map(Blob::getName).collect(Collectors.toList());
+    for (int i = lastRevision - 1; i < lastRevision + 3; i++) {
       if (!Revision.isCurrentRevision(i)) continue;
 
       LOGGER.info("Detected current revision as: " + i);
@@ -55,16 +65,5 @@ public class Application implements BackgroundFunction<Message> {
       downloadAndInsert(storage, i);
       break;
     }
-  }
-
-  private static boolean alreadyHave(List<String> blobs, int revision) {
-    String objectName = "osrs-" + revision + ".jar";
-    return blobs.contains(objectName);
-  }
-
-  private static void downloadAndInsert(Storage storage, int revision) {
-    BlobId blobId = BlobId.of(GCS_BUCKET, String.format("osrs-%d.jar", revision));
-    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-    storage.create(blobInfo, Gamepack.getJarBytes());
   }
 }
